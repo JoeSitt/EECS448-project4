d2e22d81f6327c697712835b3944a4b61f976584
AstralA <AstralA@users.noreply.github.com>
2017-11-01 10:19:45 -0500

    Add files via upload

---
+++ board.js
@@ -0,0 +1,477 @@
+/**
+* Encapsulates a game of Connect-4
+*
+* Contains an int[][] representing the game board. The grid is an array of rows,
+* meaning items are indexed as grid[row][column].
+*/
+class Game {
+
+    /**
+    * Initializes grid and starting player based on user specification. whether
+    * or not player 1 is to make first move is set and defaulted to when board
+    * is reset.
+    */
+    constructor(doesPlayer1Start) {
+        if (doesPlayer1Start) {
+            this.turn = 1;
+        } else {
+            this.turn = 2;
+        }
+
+        this.player1Starts = doesPlayer1Start;
+        this.grid = this.makeGrid(6, 7, 0);
+    }
+
+    /**
+    * Returns the transpose of the grid. As grid items are accessed via,
+    * grid[row][column], the returned tranpose can have its items accessed via
+    * returned_grid[column][row]
+    */
+    getTransposedGrid() {
+        return this.grid[0].map((col, i) => this.grid.map(row => row[i]));
+    }
+
+    /**
+    * Resets game board, resetting turn.
+    */
+    reset() {
+        this.grid = this.makeGrid(6, 7, 0);
+		
+		//modified to make the front end work
+		var elementbut  = document.getElementsByTagName("button");
+		for (var i = 0; i < (elementbut.length); i++) {
+				elementbut[i].value = 0;		
+		}
+		turns.innerHTML = "<h3>Player 1's Turn</h3>";
+		
+        if (this.player1Starts) {
+            this.turn = 1;
+        } else {
+            this.turn = 2;
+        }
+    }
+
+    /**
+    * Returns a 2d array containing content passed.
+    */
+    makeGrid(height, width, content) {
+        var outArr = [];
+        for (var i = 0; i < height; i++) {
+            var row = [];
+            for (var j = 0; j < width; j++) {
+                row.push(content);
+            }
+            outArr.push(row);
+        }
+        return outArr;
+    }
+
+    /**
+    * Switches current player to other.
+    */
+    switchTurn() {
+        if (this.turn == 1) {
+            this.turn = 2;
+        } else if (this.turn == 2) {
+            this.turn = 1;
+        } else {
+            console.log("ERROR");
+        }
+    }
+
+    /**
+    * Places player's whose turn it is's representation in spot passed.
+    * Returns true if placement was succesful (column was not full), else false.
+    * Switch turn to opposing player.
+    */
+    move(spot) {
+        if (this.grid[0][spot] != 0) {
+            return false;
+        } else {
+            for (var j = 0; j < 6; j++) {
+                if (this.grid[j][spot] != 0) {
+                    this.grid[j - 1][spot] = this.turn;
+                    this.switchTurn();
+                    return true;
+                }
+            }
+
+            this.grid[5][spot] = this.turn;
+            this.switchTurn();
+            return true;
+        }
+    }
+
+    /**
+    * Check for '\' diagonals of four for player.
+    */
+    checkMajorDiags(player) {
+
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (this.grid[i][j] == player && this.grid[i + 1][j + 1] == player && this.grid[i + 2][j + 2] == player && this.grid[i + 3][j + 3] == player) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+    * Check '/' diagonals of four for players.
+    */
+    checkMinorDiags(player) {
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (this.grid[i + 3][j] == player && this.grid[i + 2][j + 1] == player && this.grid[i + 1][j + 2] == player && this.grid[i][j + 3] == player) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+    * Check rows for player.
+    */
+    checkRows(player) {
+
+        for (var i = 0; i < 6; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (this.grid[i][j] == player && this.grid[i][j + 1] == player && this.grid[i][j + 2] == player && this.grid[i][j + 3] == player) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+    * Check columns for player.
+    */
+    checkCols(player) {
+
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 7; j++) {
+                if (this.grid[i][j] == player && this.grid[i + 1][j] == player && this.grid[i + 2][j] == player && this.grid[i + 3][j] == player) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+    * Returns whether or not the game is in a tied state.
+    */
+    isTie() {
+        for (var i = 0; i < 6; i++) {
+            if (this.grid[0][i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+    * Returns whether or not the player passed has won.
+    */
+    hasPlayerWon(player) {
+        return this.checkCols(player) || this.checkRows(player) || this.checkMajorDiags(player) || this.checkMinorDiags(player);
+    }
+
+    /**
+    * Returns integer representing current state of game.
+    *
+    * -1 : Game is in progress
+    *  0 : Game has ended and resulted in a tie
+    *  1 : Game has ended and resulted in player 1 winning
+    *  2 : Game has ended and resulted in player 2 winning
+    */
+    getState() {
+        if (this.isTie()) {
+            return 0;
+        } else if (this.hasPlayerWon(1)) {
+            return 1;
+        } else if (this.hasPlayerWon(2)) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+};
+
+// Used to store state of game. Red is treated as plauer 1 and starts
+var board = new Game(true);
+
+/**
+* Resets game. Both external and internal board representations are reset.
+*/
+function reset() {
+    board.reset();
+    update();
+}
+
+/**
+* Returns integer representing current state of game.
+*
+* -1 : Game is in progress
+*  0 : Game has ended and resulted in a tie
+*  1 : Game has ended and resulted in red winning
+*  2 : Game has ended and resulted in black winning
+*/
+function state() {
+    return board.getState();
+}
+
+/**
+* Given that the game is not over, the current player moves in the spot passed.
+*/
+function move(spot) {
+    if (board.getState() != -1) {
+		
+		//modified to make the frontend work
+		var disc = document.getElementById("turns");
+		if (board.getState() == 1) {
+			disc.innerHTML = "<h3>Game Over. Player 1 Won.</h3>";
+		}
+		else if (board.getState() == 2) {
+			disc.innerHTML = "<h3>Game Over. Player 2 Won.</h3>";
+		}
+		else {
+			disc.innerHTML = "<h3>Game Over. Game Tied.</h3>";
+		}
+		var gameEnd = document.getElementById("resetboard");
+		gameEnd.value = 1;
+		
+        return;
+    }
+
+    if (!board.move(spot)) {
+        alert("ERROR: Column is full!");
+    }
+    
+    update();
+}
+
+/**
+* Given that the game is not over, the current player places a random move.
+*/
+function moveRandom() {
+    if (board.getState() != -1) {
+		
+        //modified to make the frontend work
+		var disc = document.getElementById("turns");
+		if (board.getState() == 1) {
+			disc.innerHTML = "<h3>Game Over. Ai 1 Won.</h3>";
+		}
+		else if (board.getState() == 2) {
+			disc.innerHTML = "<h3>Game Over. Ai 2 Won.</h3>";
+		}
+		else {
+			disc.innerHTML = "<h3>Game Over. Game Tied.</h3>";
+		}
+		var gameEnd = document.getElementById("resetboard");
+		gameEnd.value = 1;
+		
+        return;
+    }
+	var disc = document.getElementById("turns");
+	disc.innerHTML = "<h3>Ai Currently Playing.</h3>";
+	
+    // Move randomly until valid move found
+    while (!board.move(Math.round(Math.random()*7) + 1)) {}
+
+    update();
+}
+
+/**
+* Updates frontend board repsentation according to Game object.
+*/
+function update() {
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 7; j++) {
+            var b = document.getElementById("b" + i + j);
+            var r = document.getElementById("r" + i + j);
+            var n = document.getElementById("n" + i + j);
+
+            if (board.grid[i][j] == 0) {
+                b.hidden = 1;
+                r.hidden = 1;
+                n.hidden = 0;
+            }
+            if (board.grid[i][j] == 1) {
+                b.hidden = 1;
+                r.hidden = 0;
+                n.hidden = 1;
+            }
+            if (board.grid[i][j] == 2) {
+                b.hidden = 0;
+                r.hidden = 1;
+                n.hidden = 1;
+            }
+        }
+    }
+}
+
+//added the buttons function
+function dropDisc(x) {
+	var disc = document.getElementById("turns");
+	var check = false;
+	var gameEnd = document.getElementById("resetboard");
+	if (gameEnd.value != 1) {
+	if (x == 1) {
+		var fir = document.getElementById("first");
+		if (fir.value < 6) {
+			check = true;
+			fir.value++;
+		}
+	}
+	else if (x == 2) {
+		var sec = document.getElementById("second");
+		if (sec.value < 6) {
+			check = true;
+			sec.value++;
+		}
+	}
+	else if (x == 3) {
+		var thi = document.getElementById("third");
+		if (thi.value < 6) {
+			check = true;
+			thi.value++;
+		}
+	}
+	else if (x == 4) {
+		var fou = document.getElementById("fourth");
+		if (fou.value < 6) {
+			check = true;
+			fou.value++;
+		}
+	}
+	else if (x == 5) {
+		var fif = document.getElementById("fifth");
+		if (fif.value < 6) {
+			check = true;
+			fif.value++;
+		}
+	}
+	else if (x == 6) {
+		var six = document.getElementById("sixth");
+		if (six.value < 6) {
+			check = true;
+			six.value++;
+		}
+	}
+	else {
+		var sev = document.getElementById("seventh");
+		if (sev.value < 6) {
+			check = true;
+			sev.value++;
+		}
+	}
+	if (((disc.value % 2) == 1) && check) {
+		move(x - 1);
+		disc.innerHTML = "<h3>Player 1's Turn</h3>";
+		disc.value++;
+		getWinner();
+	}
+	else if (check) {
+		move(x - 1);
+		disc.innerHTML = "<h3>Player 2's Turn</h3>";
+		disc.value++;
+		getWinner();
+	}
+	if (disc.value == 42) {
+		disc.innerHTML = "<h3>Game Over.</h3>";
+	}
+	}
+	return;
+}
+//added a function for player vs ai
+function fightAi(x) {
+	var disc = document.getElementById("turns");
+	var check = false;
+	var gameEnd = document.getElementById("resetboard");
+	if (gameEnd.value != 1) {
+	if (x == 1) {
+		var fir = document.getElementById("first");
+		if (fir.value < 6) {
+			check = true;
+			fir.value++;
+		}
+	}
+	else if (x == 2) {
+		var sec = document.getElementById("second");
+		if (sec.value < 6) {
+			check = true;
+			sec.value++;
+		}
+	}
+	else if (x == 3) {
+		var thi = document.getElementById("third");
+		if (thi.value < 6) {
+			check = true;
+			thi.value++;
+		}
+	}
+	else if (x == 4) {
+		var fou = document.getElementById("fourth");
+		if (fou.value < 6) {
+			check = true;
+			fou.value++;
+		}
+	}
+	else if (x == 5) {
+		var fif = document.getElementById("fifth");
+		if (fif.value < 6) {
+			check = true;
+			fif.value++;
+		}
+	}
+	else if (x == 6) {
+		var six = document.getElementById("sixth");
+		if (six.value < 6) {
+			check = true;
+			six.value++;
+		}
+	}
+	else {
+		var sev = document.getElementById("seventh");
+		if (sev.value < 6) {
+			check = true;
+			sev.value++;
+		}
+	}
+	if (check) {
+		move(x - 1);
+		getWinner();
+		moveRandom();
+		disc.innerHTML = "<h3>Player 1's Turn</h3>";
+		getWinner();
+	}
+	}
+	return;
+}
+//added a function to determine winner
+function getWinner() {
+	var disc = document.getElementById("turns");
+	var gameEnd = document.getElementById("resetboard");
+	if (board.getState() != -1) {
+		if (board.getState() == 1) {
+			disc.innerHTML = "<h3>Game Over. Player 1 Won.</h3>";
+		}
+		else if (board.getState() == 2) {
+			disc.innerHTML = "<h3>Game Over. Player 2 Won.</h3>";
+		}
+		else {
+			disc.innerHTML = "<h3>Game Over. Game Tied.</h3>";
+		}
+		gameEnd.value = 1;
+	}
+	return;
+}
---
+++ getboard.js
@@ -0,0 +1,80 @@
+	function Create() {
+	var choice = document.getElementsByName("q1");
+	var picked;
+	for (var i = 0; i < choice.length; i++) {
+		if (choice[i].checked) {
+		picked = choice[i].value;
+        break;
+		}
+	}
+	var buttons = '<br><center><button id="first" value="0" onclick="dropDisc(1)">First Column.</button><button id="second" value="0" onclick="dropDisc(2)">Second Column.</button><button id="third" value="0" onclick="dropDisc(3)">Third Column.</button><button id="fourth" value="0" onclick="dropDisc(4)">Fourth Column.</button><button id="fifth" value="0" onclick="dropDisc(5)">Fifth Column.</button><button id="sixth" value="0" onclick="dropDisc(6)">Sixth Column.</button><button id="seventh" value="0" onclick="dropDisc(7)">Seventh Column.</button><br><br><button id="resetboard" value="0" onclick="reset()">Reset Board.</button><br></center>';
+	var buttons1 = '<br><center><button id="first" value="0" onclick="fightAi(1)">First Column.</button><button id="second" value="0" onclick="fightAi(2)">Second Column.</button><button id="third" value="0" onclick="fightAi(3)">Third Column.</button><button id="fourth" value="0" onclick="fightAi(4)">Fourth Column.</button><button id="fifth" value="0" onclick="fightAi(5)">Fifth Column.</button><button id="sixth" value="0" onclick="fightAi(6)">Sixth Column.</button><button id="seventh" value="0" onclick="fightAi(7)">Seventh Column.</button><br><br><button id="resetboard" value="0" onclick="reset()">Reset Board.</button><br></center>';
+	var turn = '</button><button class="button" value="0" id="turns"><h3>Player 1\'s Turn</h3></button><br>';
+	var choice = '<form onsubmit="Create()"><h2>What mode would you like to play?</h2><br><fieldset id="q1"><h4><input name="q1" value="1" id="A" type="radio"> A. Player vs Ai<br><input name="q1" value="2" id="B" type="radio" checked="checked"> B. 2 players<br><input name="q1" value="3" id="C" type="radio"> C. Ai vs Ai<br></h4></fieldset><br><input type="submit" name="submit" value="Submit.">	</form>';
+	var gameMode = 0;
+	if (picked == 1) {
+		document.body.innerHTML = '<center><h3 id="1A">You picked Player vs Ai.</h3>' + turn + '</center>';
+		gameMode = 1;
+	}
+	else if (picked == 2) {
+		document.body.innerHTML = '<center><h3 id="PP">You picked Player vs Player.</h3>' + turn + '</center>';
+		gameMode = 2;
+	}
+	else {
+		document.body.innerHTML = '<center><h3 id="AA">You picked Ai vs Ai.</h3>' + turn + '</center>';
+		gameMode = 3;
+	}
+
+	//var row = document.createElement("tr");
+	//document.getElementById("tableE").appendChild(row);
+	var tableE = document.createElement("table");
+	tableE.style.border = "1px solid black";
+	tableE.id = "Table";
+	/*var imge = document.createElement("img");
+       imge.src = "https://people.eecs.ku.edu/~j024s900/project3/nodot.png";
+       imge.height = "50"; 
+       imge.width = "50";
+      
+	var imge1 = document.createElement("img");
+	   imge1.src = "https://people.eecs.ku.edu/~j024s900/project3/bdot.png";
+       imge1.height = "50";
+       imge1.width = "50";
+       imge1.hidden = "1";
+	   
+    var imge2 = document.createElement("img");   
+	   imge2.src = "https://people.eecs.ku.edu/~j024s900/project3/reddot.png";
+       imge2.height = "50";
+       imge2.width = "50";
+       imge2.hidden = "1";*/
+	   
+	for (var i = 0; i < 6; i++) {
+		var row = tableE.insertRow(-1);
+		for (var j = 0; j < 7; j++) {	
+			var id = "n" + i + j;
+			var id1 = "b" + i + j;
+			var id2 = "r" + i + j;
+			var b = '<img src="https://people.eecs.ku.edu/~j024s900/project3/nodot.png" height="100" width="100" id="' + id + '">' + '<img src="https://people.eecs.ku.edu/~j024s900/project3/bdot.png" height="100" width="100" hidden="1" id="' + id1 + '">' + '<img src="https://people.eecs.ku.edu/~j024s900/project3/reddot.png" height="100" width="100" hidden="1" id="' + id2 + '">';
+			var cell1 = row.insertCell(-1);
+			cell1.style.border = "1px solid black";
+			cell1.innerHTML = b;
+			//cell1.appendChild(imge1);
+			//cell1.appendChild(imge2);
+		}
+	}
+	document.body.appendChild(tableE);
+	
+	if (gameMode == 1) {
+		document.body.innerHTML += buttons1;
+	}
+	else if (gameMode == 2) {
+		document.body.innerHTML += buttons;		
+	}
+	else {
+			for (var i = 0; i < 42; i++) {
+			(function (i) {
+				setTimeout(moveRandom, 1000*i);
+			})(i);
+		}
+	}
+	
+	}		
