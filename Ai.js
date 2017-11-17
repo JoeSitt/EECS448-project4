22f17757055f347525756bfba097b6097ad36e26
JoeSitt <31551266+JoeSitt@users.noreply.github.com>
2017-11-17 11:01:59 -0600

    Ais

---
+++ Ai.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+class Ai {
+  constructor() {
+    this.m=function( gameboard){}
+    this.name="no name";
+  }
+}
+    var x=-1;
+class left extends Ai{
+  constructor(){
+    super();
+    this.m=function( gameboard){
+
+        x=x+1;
+        if(x==7){
+          x=0;
+        }
+      return(x);
+    }
+
+    this.name="Lefto boto";
+  }
+
+}
+
+class randobot extends Ai{
+  constructor() {
+    super();
+    this.m=function(gameboard){
+      return getRandInt(0, 7);
+    }
+      this.name="Randobot";
+  }
+
+}
